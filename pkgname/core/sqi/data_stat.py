63f1e4ba2b9b2ebc5fd52eabcfe3cb99bda8a1fb
sky-sailor <wang-jingkai@hotmail.com>
2023-07-11 14:07:51 +0800

    uncomment 2 ways of calculation

--- pkgname/core/sqi/data_stat.py
+++ pkgname/core/sqi/data_stat.py
@@ -1,19 +1,19 @@
 """
-Description of the Dataset (Size, Mean, Std)
+Description of the Dataset (size, mean, std)
 Signal analysis and classification of photoplethysmography (PPG) waveforms for predicting clinical outcomes
 """
 
 import pandas as pd
 import os
+
 filepath = 'D:/IC Final Year Project/OUCRU/adults/01NVa-003-2162/PPG'
 filename = '20200720T140507.193+0700.csv'
-signals = pd.read_csv(os.path.join(filepath, filename))  # include the number of rows to load
-# print("\nLoaded signals:")
+signals = pd.read_csv(os.path.join(filepath, filename))
 print(signals.shape)
 
-# Mean and Std calculation in 3 ways
-# mean = signals.PLETH.mean()
-# mean2 = signals['PLETH'].mean()
-# std = signals.PLETH.std()
-# std2 = signals['PLETH'].std()
+# mean and std calculation in 3 ways
+mean1 = signals.PLETH.mean()
+mean2 = signals['PLETH'].mean()
+std1 = signals.PLETH.std()
+std2 = signals['PLETH'].std()
 signals['PLETH'].describe()
